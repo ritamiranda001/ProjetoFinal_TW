1d65fc7a16db03fdccf158c463fa799b1f8d2f2f
Keila dos Santos <keilopesantos@gmail.com>
2026-05-27 15:55:03 +0100

    feat: setup routing and navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { RecipesComponent } from './pages/recipes/recipes.component';
+import { RecipeDetailComponent } from './pages/recipe-detail/recipe-detail.component';
+import { FavoritesComponent } from './pages/favorites/favorites.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'recipes', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'recipes', component: RecipesComponent },
+  { path: 'recipes/:id', component: RecipeDetailComponent },
+  { path: 'favorites', component: FavoritesComponent },
+  { path: '**', redirectTo: 'recipes' }
+];
